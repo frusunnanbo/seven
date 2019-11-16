4e9aa670c9b6f789ab33833b02a1701d5c3927b3
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2019-11-16 17:41:33 +0100

    Indexed greppin'

--- ruby/simple_grep.rb
+++ ruby/simple_grep.rb
@@ -5,6 +5,8 @@ pattern = Regexp.new(ARGV[1])
 
 File.open(ARGV[0], 'r') do |file|
   file.readlines
-    .select { |line| line =~ pattern }
-    .each { |line| puts "#{line}" }
+    .map
+    .with_index
+    .select { |indexed_line| indexed_line.first =~ pattern }
+    .each { |indexed_line| puts "#{indexed_line.last}: #{indexed_line.first}" }
 end
