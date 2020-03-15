3d339dbe411efdff7ac460aed22848a54df44095
Pia Fåk Sunnanbo <pia.fak.sunnanbo@gmail.com>
2020-03-15 20:35:51 +0100

    Sorting with a custom comparator

--- haskell/sorting.hs
+++ haskell/sorting.hs
@@ -5,3 +5,13 @@ module Sorting where
 
   sort :: [Integer] -> [Integer]
   sort = foldl insert []
+
+  insertComparing :: (Integer -> Integer -> Bool) -> [Integer] -> Integer -> [Integer]
+  insertComparing comparator [] element = [element]
+  insertComparing comparator (head:tail) element =
+    if (comparator element head)
+      then element:head:tail
+      else (head:(insertComparing comparator tail element))
+
+  sortWithComparator :: (Integer -> Integer -> Bool) -> [Integer] -> [Integer]
+  sortWithComparator comparator = foldl (\acc el -> insertComparing comparator acc el) []
